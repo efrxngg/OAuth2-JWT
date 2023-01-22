50e742c863ffaa49d9999a598cc62bf098c60fde
Efren Galarza <efrenpgc2602@gmail.com>
2023-01-21 19:33:16 -0500

    added h2 db

--- src/main/java/com/empresax/security/config/MariaDBConfig.java
+++ src/main/java/com/empresax/security/config/MariaDBConfig.java
@@ -2,6 +2,7 @@ package com.empresax.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+@Profile(value = "prod")
 @Configuration
 @EnableTransactionManagement
 public class MariaDBConfig {
@@ -17,8 +19,8 @@ public class MariaDBConfig {
     @Bean
     public DataSource getSecurityEmpresaxDataSource() {
         DriverManagerDataSource source = new DriverManagerDataSource();
-        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        source.setUrl("jdbc:mysql://127.0.0.1:3307/security_empresax");
+        source.setDriverClassName("org.mariadb.jdbc.Driver");
+        source.setUrl("jdbc:mariadb://127.0.0.1:3307/security_empresax");
         source.setUsername("root");
         source.setPassword("root1234");
         return source;
