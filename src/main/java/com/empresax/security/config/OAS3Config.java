b174decd6c7b83582d032f41b58028aa28f60b60
Efren Galarza <efrenpgc2602@gmail.com>
2023-01-22 20:41:31 -0500

    update open api specification

--- src/main/java/com/empresax/security/config/OAS3Config.java
+++ src/main/java/com/empresax/security/config/OAS3Config.java
@@ -1,9 +1,15 @@
 package com.empresax.security.config;
 
-import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SecurityScheme(
@@ -13,5 +19,25 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
         scheme = "bearer"
 )
 public class OAS3Config {
+
+    @Bean
+    public OpenAPI infoOpenApi() {
+        return new OpenAPI()
+                .info(new Info().title("Login Service")
+                        .description("""
+                                Autenticacion y Autorizacion mediante JWT firmado con **RSA256** public y private key generados con el **JDK 17**.
+                                                                
+                                Para mas informacion puedes visitar mi repositorio en Github **OAuth2-JWT**.
+                                      """)
+                        .version("v1")
+                        .license(
+                                new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                        .contact(new Contact()
+                                .name("@efrxngg")
+                                .url("https://efrxngg.github.io/")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub")
+                        .url("https://github.com/efrxngg"));
+    }
 
 }
